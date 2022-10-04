c901e354e8c86868f5f571cffed44feda37ad969
Lance Parsons <lparsons@princeton.edu>
2022-10-04 15:40:27 -0400

    Use JS Standard Style

--- static/js/htmlSorter.js
+++ static/js/htmlSorter.js
@@ -1,9 +1,9 @@
 // sort column based on HTML values
 // ref: https://github.com/wenzhixin/bootstrap-table/issues/461
-function htmlSorter(a, b) {
-    var a = $(a).text();
-    var b = $(b).text();
-    if(a < b) return -1;
-    if(a > b) return 1;
-    return 0;
+export function htmlSorter (a, b) {
+  const atext = $(a).text()
+  const btext = $(b).text()
+  if (atext < btext) return -1
+  if (atext > btext) return 1
+  return 0
 }
